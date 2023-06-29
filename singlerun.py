46878930ca4bb205f60bfdd95d76d6d2dc985527
Alex Haskins <alexanderwantshislibraryback@gmail.com>
2023-06-28 22:18:36 -0400

    updating to selenium 4 - bugged out.

--- singlerun.py
+++ singlerun.py
@@ -3,9 +3,10 @@
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service  as ChromeService
 import time, string, subprocess, json
 
-print("First Pass, removing punctuation.")
+print('First Pass, removing punctuation.')
 # Scrub oldnorse_input.txt for punctuation
 def remove_punctuation(punc_input):
   # define punctuation to remove, using a known constant from the string library
@@ -13,30 +14,38 @@ def remove_punctuation(punc_input):
   # return the input string with all punctuation marks removed
   return ''.join([char for char in punc_input if char not in punct])
 # Open the input file in read mode, assign it
-with open('texts/oldnorse_input.txt', 'r') as input_file:
+with open('oldnorse_input.txt', 'r') as input_file:
   # read the entire contents of the file as a string
   punc_input = input_file.read()
 # Call the remove_punctuation function
 punc_output = remove_punctuation(punc_input)
 # open the output file in write mode
-with open('texts/oldnorse_nopunc.txt', 'w') as output_file:
+with open('1-oldnorse_nopunc.txt', 'w') as output_file:
   # write the output string to the file
   output_file.write(punc_output)
 print('First pass: Complete!')
 
 print('Second pass, cross referencing runic.is')
-driver_path = "chromedriver/chromedriver"
-brave_path = "/usr/bin/brave-browser"
-option = webdriver.ChromeOptions()
-option.binary_location = brave_path
-#option.add_argument("--headless=new")
-driver = webdriver.Chrome(executable_path=driver_path, chrome_options=option)
+driver_path = 'chromedriver/chromedriver'
+brave_path = '/usr/bin/brave-browser'
+####HACK - Updating to Selenium 4.
+
+# Set the path to the ChromeDriver executable
+service = ChromeService(driver_path)
+# Configure Chrome options
+options = webdriver.ChromeOptions()
+options.binary_location = brave_path
+# options.add_argument("--headless")
+# Create the Chrome WebDriver using the Service and Options
+driver = webdriver.Chrome(service=service, options=options)
+
+####HACK
 #open our source
 driver.get('https://runic.is/')
 #Let it load
 time.sleep(3)
 # Loop through each input string
-with open('texts/oldnorse_nopunc.txt') as input_file:
+with open('1-oldnorse_nopunc.txt') as input_file:
     oldnorse_array = input_file.readlines()
 for runa_string_in in oldnorse_array:
     # Find the input field on the page and enter the text from the input string
@@ -59,7 +68,7 @@ for runa_string_in in oldnorse_array:
     input_field.clear()
     time.sleep(1)
     # Write the output text to the output.txt file
-    with open('texts/futhark_raw.txt', 'a') as output_file:
+    with open('2-futhark_raw.txt', 'a') as output_file:
         output_file.write(runo_string + '\n')
 #KILL IT WITH FIRE
 driver.close()
@@ -71,7 +80,7 @@ print('Third pass: Fixing the letter-typos')
 with open('dictionaries/letterswap.json') as f:
     ls_dict = json.load(f)
 # Open the input file
-with open('texts/futhark_raw.txt', 'r') as g:
+with open('2-futhark_raw.txt', 'r') as g:
     # Read the entire file into a string
     s = g.read()
 # Initialize an empty list to store the modified characters
@@ -86,7 +95,7 @@ for c in s:
 # Join the modified characters into a modified string
 modified_s = ''.join(modified_chars)
 # Open the output file
-with open('texts/futhark_allrunes.txt', 'w') as f:
+with open('3-futhark_allrunes.txt', 'w') as f:
     # Write the modified string to the output file
     f.write(modified_s)
 print('Third pass: Complete!')
@@ -95,7 +104,7 @@ print('Fourth Pass, spell-check')
 with open('dictionaries/spellcheck.json') as f:
     sc_dict = json.load(f)
 # Open the input file
-with open('texts/futhark_allrunes.txt', 'r') as f:
+with open('3-futhark_allrunes.txt', 'r') as f:
     # Read the file line by line
     lines = f.readlines()
 # Initialize an empty list to store the modified lines
@@ -114,11 +123,26 @@ for line in lines:
         # Otherwise, keep the word as it is
         modified_words.append(word)
     # Join the modified words into a modified line
-    modified_line = ' '.join(modified_words)
+    modified_line = '᛫'.join(modified_words) # I like the dot-space better. Looks somehow too thin. Needs meat.
     # Add the modified line to the list of modified lines
     modified_lines.append(modified_line)
 # Open the output file
-with open('texts/CLEAN_futhark.txt', 'w') as f:
+with open('4-spchked_futhark.txt', 'w') as f:
     # Write the modified lines to the output file
     f.writelines(modified_lines)
-print("Fourth pass: Complete!")
+print('Fourth pass: Complete!')
+
+print('Fifth Pass, remove empty lines.')
+# Open the input file in read mode
+with open('4-spchked_futhark.txt', 'r') as file:
+    # Read all lines into a list
+    lines = file.readlines()
+# Open the output file in write mode
+with open('FINAL.txt', 'w') as file:
+    # Loop through each line
+    for line in lines:
+        # Check if the line is empty (contains only whitespace)
+        if line.strip():
+            # Write non-empty lines to the output file
+            file.write(line)
+print('Final Pass Complete.')
